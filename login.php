79bd376da72f771b4fbfe1879b3560ae3ab4e5e1
noah <noahcwoods@gmail.com>
2018-10-20 00:56:48 -0400

    ...

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+
+//All pages will redirect to Login
+//After successfull login, go to index.php
+//Include a "Continue as guest" option
+//Create profiles and profile.php
+//email verification?
+//Finish NOAHOS, add noahos.php as example of work
+//resume.php? add resume
+//create database on server (noahcwoods.com) userid, username, password, email, phone?, description, firstname, lastname
+
+ ?>
