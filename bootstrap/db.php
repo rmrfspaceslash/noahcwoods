922db4c1221795b3a6bfdf19ab06a630ce5cb0be
noah <noahcwoods@gmail.com>
2018-10-24 12:07:46 -0400

    ...

--- bootstrap/db.php
+++ bootstrap/db.php
@@ -1,11 +1,8 @@
 <?php
 
 $db = new SQLite3('../SQLite/php.db');
-
-$sql = "INSERT into users (email, username, password) VALUES ("noahcwoods@gmail.com", "noahcwoods", "testpass")";
-
-$db->exec($sql);
+$db->exec("INSERT into users (email, username, password) VALUES ("noahcwoods@gmail.com", "noahcwoods", "testpass")");
 
 echo "hello";
 
- ?>
+?>
