d6ca92d652aca76ba6297c299ba88e5613a46290
noah <noahcwoods@gmail.com>
2018-10-23 20:49:36 -0400

    ...

--- bootstrap/register.php
+++ bootstrap/register.php
@@ -51,7 +51,7 @@
     <!-- Register -->
     <header class="masthead">
       <div class="container d-flex h-100 align-items-center">
-        <div class="mx-auto text-center bg-black">
+        <div class="mx-auto text-center">
           <h3 class="display-4 mx-auto my-0 text-uppercase text-white">Register Now</h3><br/>
           <form action="register.php" method="post">
             <div class="mx-auto text-center">
