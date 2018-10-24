a4f81774f59c564379e8c1dd289b22e6befe4160
noah <noahcwoods@gmail.com>
2018-10-24 11:57:39 -0400

    ...

--- bootstrap/db.php
+++ bootstrap/db.php
@@ -2,12 +2,8 @@
 
 $db = new PDO('sqlite:../SQLite/php.db');
 
-$memory_db = new PDO('sqlite::memory:');
-
 $sql = "INSERT into users (email, username, password) VALUES ("noahcwoods@gmail.com", "noahcwoods", "testpass")";
 
-$sql = $db->prepare($sql);
-
-$db->execute($sql);
+$db->query($sql);
 
  ?>
