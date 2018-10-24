7b216c72fa328d15ca0bc615f51ed03870f5f734
noah <noahcwoods@gmail.com>
2018-10-24 11:34:57 -0400

    ...

--- bootstrap/db.php
+++ bootstrap/db.php
@@ -4,8 +4,10 @@ $pdo = new PDO('sqlite3:SQLite/php.db');
 
 $sql = "INSERT into users (email, username, password) VALUES ("noahcwoods@gmail.com", "noahcwoods", "testpass")";
 
-$result = $myPDO->query($sql)
+$result = $myPDO->query($sql);
 
 echo $result;
+
+echo "1234";
 
  ?>
