c7d4d35c286102297a474b93abd921f3a4c128e2
noah <noahcwoods@gmail.com>
2018-10-25 18:40:18 -0400

    ...

--- bootstrap/login.php
+++ bootstrap/login.php
@@ -60,6 +60,7 @@ if ($_POST['email'] != null && $_POST['password'] != null) {
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
