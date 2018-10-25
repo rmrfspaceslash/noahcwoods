db62a5ab76867a9ccfa8a53bc182dd540f9ed44f
noah <noahcwoods@gmail.com>
2018-10-25 19:03:02 -0400

    ...

--- bootstrap/profile.php
+++ bootstrap/profile.php
@@ -71,14 +71,11 @@ if ($_SESSION['loggedin'] = null) {
     <header class="masthead">
       <div class="container d-flex h-100 align-items-center">
         <div class="">
-          <img src="img/demo-image-01.jpg" class="img-thumbnail col-lg-4 col-md-4 col-sm-6 col-xs-12">
+          <img src="img/demo-image-01.jpg" class="mx-auto img-thumbnail col-lg-4 col-md-4 col-sm-6 col-xs-12">
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 jumbotron">
             <label>First Name: </label><br>
             <label>Last Name: </label><br>
             <label>About you: </label><br>
-          </div>
-          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 jumbotron">
-
           </div>
         </div>
       </div>
