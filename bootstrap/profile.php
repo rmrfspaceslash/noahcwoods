f1f09d505f9308e9d034e45d6edb72c778ec6ca5
noah <noahcwoods@gmail.com>
2018-10-25 19:01:43 -0400

    ...

--- bootstrap/profile.php
+++ bootstrap/profile.php
@@ -73,9 +73,12 @@ if ($_SESSION['loggedin'] = null) {
         <div class="mx-auto text-center">
           <img src="img/demo-image-01.jpg" class="img-thumbnail col-lg-4 col-md-4 col-sm-6 col-xs-12">
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 jumbotron">
-            <label>First Name: </label>
-            <label>Last Name: </label>
-            <label>About you: </label>
+            <label>First Name: </label><br>
+            <label>Last Name: </label><br>
+            <label>About you: </label><br>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 jumbotron">
+
           </div>
         </div>
       </div>
