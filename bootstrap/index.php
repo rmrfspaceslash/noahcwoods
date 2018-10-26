d9c1da9a300ebaec99477c8b4f88edeea6397ea0
noah <noahcwoods@gmail.com>
2018-10-25 20:51:48 -0400

    ...

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -44,7 +44,7 @@ if ($_POST['register'] != null) {
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Noah C. Woods</a>
+        <a class="navbar-brand nav-link js-scroll-trigger" href="#page-top">Noah C. Woods</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
