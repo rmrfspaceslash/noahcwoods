d6c0f6c74e133dad197e3ff8704977196ea4d797
noah <noahcwoods@gmail.com>
2018-10-25 20:55:53 -0400

    ...

--- bootstrap/register.php
+++ bootstrap/register.php
@@ -60,7 +60,7 @@ if ($_POST['email'] != null && $_POST['username'] != null && $_POST['password'] 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="index.php">Noah C. Woods</a>
+        <a class="nav-link navbar-brand js-scroll-trigger" href="index.php">Noah C. Woods</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
